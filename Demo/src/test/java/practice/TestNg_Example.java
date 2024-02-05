626610c1a7b266c291e768a6f27bd335f4cc3fb4
Charumathy159 <charumathy159@gmail.com>
2024-02-05 06:14:28 +0000

    code changes

--- Demo/src/test/java/practice/TestNg_Example.java
+++ Demo/src/test/java/practice/TestNg_Example.java
@@ -1,5 +1,76 @@
 package practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestNg_Example {
 
+	WebDriver driver;
+	@Test(priority=0)
+	public void LaunchBrowser()
+	{
+	 driver = new ChromeDriver();
+	 driver.manage().window().maximize();
+	}
+	@Test(priority=1)
+	public void LoadPage()
+	{
+		driver.get("https://demo.automationtesting.in/Register.html");
+	}
+	@Test(priority=2)
+	public void Fullname()
+	{
+		WebElement Firstname = driver.findElement(By.xpath("//input[@ng-model='FirstName']"));
+		Firstname.sendKeys("Charumathy");
+		WebElement LastName = driver.findElement(By.xpath("//input[@ng-model='LastName']"));
+		LastName.sendKeys("V");
+		
+	}
+	
+	@Test(priority=3)
+	public void Address()
+	{
+		WebElement Address = driver.findElement(By.xpath("//textarea[@ng-model='Adress']"));
+		Address.sendKeys("Guduvancheri");
+		
+	}
+	
+	@Test(priority=4)
+	public void Email()
+	{
+		WebElement Email = driver.findElement(By.xpath("//input[@ng-model='EmailAdress']"));
+		Email.sendKeys("charu@gmail.com");
+	}
+	@Test(priority=5)
+	public void Phone()
+	{
+		WebElement Phone = driver.findElement(By.xpath("//input[@ng-model='Phone']"));
+		Phone.sendKeys("9876543210");
+	}
+	@Test(priority=6)
+	public void Gender()
+	{
+		WebElement GenderButton = driver.findElement(By.xpath("//input[@value='FeMale']"));
+		
+		GenderButton.click();
+	}
+	@Test(priority=7)
+	public void Hobbies()
+	{
+		WebElement Movies = driver.findElement(By.xpath("//input[@value='Movies']"));
+		Movies.click();
+		
+	}
+	@Test(priority=8)
+	public void Languages()
+	{
+		WebElement Languages = driver.findElement(By.xpath("//div[@id='msdd']"));
+		Languages.sendKeys("English,Tamil");
+		
+		
+	}
+	
 }
